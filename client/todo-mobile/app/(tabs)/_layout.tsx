394c045fb3f68a1eebccbf668f523070dc639b53
5imyboy <simonyong05@gmail.com>
2026-07-07 21:13:46 -0700

    mobile: use new library to make tabs swipe-able

--- client/todo-mobile/app/(tabs)/_layout.tsx
+++ client/todo-mobile/app/(tabs)/_layout.tsx
@@ -1,11 +1,25 @@
-import { Tabs, useRouter } from "expo-router";
+import { useRouter } from "expo-router";
+import { useState } from "react";
 import { Button, Image, Pressable, StyleSheet, Text, View } from "react-native";
 import { useAuth } from "../../contexts/AuthContext";
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { SafeAreaView } from "react-native-safe-area-context";
+import Not_Started from "./not-started";
+import In_Progress from "./in-progress";
+import Finished from "./finished";
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function TabLayout() {
   const router = useRouter();
   const { token, setToken } = useAuth();
   const ICON_SIZE = 50;
+  const titles: Record<string, string> = {
+    "not-started": "Not Started",
+    "in-progress": "In Progress",
+    "finished": "Finished",
+  };
+  const [title, setTitle] = useState("Not Started");
   const NULL_TASK = {
     task: JSON.stringify({
       taskId: 0,
@@ -23,31 +37,34 @@ export default function TabLayout() {
   };
 
   return (
-    <View style={{ flex: 1 }}>
-      <Tabs screenOptions={{
-        headerRight: () => (
-          <View style={{flexDirection: "row", gap: 8 }}>
-            <Pressable
-              style={styles.addButton}
-              onPress={() => router.push({
-                pathname: "/task-form",
-                params: NULL_TASK
-              })}
-            >
-              <Text style={styles.addButtonText}>+</Text>
-            </Pressable>
-            {token
-              ? <Button title="Logout" onPress={handleLogout} />
-              : <Button title="Sign In" onPress={() => router.push("/login")} />
-            }
-          </View>
-        )
-      }}>
-        <Tabs.Screen name="not-started" options={{ title: "Not Started", tabBarIcon: () => <Image source={require("../../assets/images/not-started.png")} style={{ width: ICON_SIZE, height: ICON_SIZE }} /> }} />
-        <Tabs.Screen name="in-progress" options={{ title: "In Progress", tabBarIcon: () => <Image source={require("../../assets/images/in-progress.png")} style={{ width: ICON_SIZE, height: ICON_SIZE }} /> }} />
-        <Tabs.Screen name="finished" options={{ title: "Finished", tabBarIcon: () => <Image source={require("../../assets/images/completed.png")} style={{ width: ICON_SIZE, height: ICON_SIZE }} /> }} />
-      </Tabs>
-    </View>
+    <SafeAreaView style={{ flex: 1 }} >
+      <View style={{ flexDirection: "row-reverse", gap: 8, padding: 12}}>
+        {token
+          ? <Button title="Logout" onPress={handleLogout} />
+          : <Button title="Sign In" onPress={() => router.push("/login")} />
+        }
+        <Pressable
+          style={styles.addButton}
+          onPress={() => router.push({
+            pathname: "/task-form",
+            params: NULL_TASK
+          })}
+        >
+          <Text style={styles.addButtonText}>+</Text>
+        </Pressable>
+        <Text style={styles.titleText}>{title}</Text>
+      </View>
+      <Tab.Navigator
+        tabBarPosition="bottom"
+        screenListeners={({ route }) => ({
+          focus: () => setTitle(titles[route.name]),
+        })}
+      >
+        <Tab.Screen name="not-started" component={Not_Started} options={{ title: "Not Started", tabBarIcon: () => <Image source={require("../../assets/images/not-started.png")} style={{ width: ICON_SIZE, height: ICON_SIZE }} /> }} />
+        <Tab.Screen name="in-progress" component={In_Progress} options={{ title: "In Progress", tabBarIcon: () => <Image source={require("../../assets/images/in-progress.png")} style={{ width: ICON_SIZE, height: ICON_SIZE }} /> }} />
+        <Tab.Screen name="finished" component={Finished} options={{ title: "Finished", tabBarIcon: () => <Image source={require("../../assets/images/completed.png")} style={{ width: ICON_SIZE, height: ICON_SIZE }} /> }} />
+      </Tab.Navigator>
+    </SafeAreaView>
   );
 }
 
@@ -67,4 +84,9 @@ const styles = StyleSheet.create({
     lineHeight: 28,
     fontWeight: "300",
   },
+  titleText: {
+    fontSize: 24,
+    fontWeight: "300",
+    marginRight: "auto"
+  }
 });
